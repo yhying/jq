2d20d21f2d969a9cee3e529725aeab75eef4ec16
18627805032@163.com <18627805032@163.com>
2020-04-19 21:34:20 +0800

    vue登录

--- 登录模板/vue登录加广告/js/1.js
+++ 登录模板/vue登录加广告/js/1.js
@@ -5,8 +5,9 @@ window.onload = function () {
             adBack: true,
             /*开屏广告显示隐藏 */
             time: 5,
-            timer: null,
-            /* 定时器 */
+            timer: null,/* 定时器 */
+            mobile:'',
+            password:''
         },
         mounted() {
             this.autoPlay()
@@ -33,6 +34,23 @@ window.onload = function () {
             jump(){
                 this.adBack = false
                 clearInterval(this.timer)
+            },
+            // 点击登录
+            login(){
+                let data={
+                    mobile:this.mobile,
+                    password:this.mobile
+                }
+                axios({
+                    method:"POST",
+                    url:'http://localhost:3001/api/login',
+                    data:JSON.stringify(data)  /* 数据序列化处理 */
+                }).then(res=>{
+                    console.log(res)
+                    if(res.data.code=='200'){
+                        window.location.href='https://github.com/yhying'
+                    }
+                })
             }
         },
 
