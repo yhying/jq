a8a44357235925b260f3be24f3d3086699951228
18627805032@163.com <18627805032@163.com>
2020-04-23 21:09:32 +0800

    懒加载和分页

--- 原生/路由懒加载加分页/js/index.js
+++ 原生/路由懒加载加分页/js/index.js
@@ -1,20 +1,44 @@
 $(function () {
+    let pageNum = 1 /* 页码 */
+    let pageSize = 4 /* 条数 */
+    showData(pageNum++,pageSize);
+    // console.log($(window))
+    // 滚动条触底 注意，html,body 不能设置100%高度，否则滚动无效
+    $(window).scroll(function () {   
+        console.log(this)
+        let scrollTop =Math.ceil($(this).scrollTop()) /* 滚动条与顶部高度 */
+        let curHeight = $(this).height() /* 当前可视页面高度 */ 
+        let totalHeight = $(document).height() /* 页面的内容总高度 */
+        // console.log(scrollTop, curHeight, totalHeight)
+        if(scrollTop+curHeight>=totalHeight){
+            alert('加载到底了')
+            showData(pageNum++,pageSize)  /* 滚到底部，页码加一 */
+        }
+    })
+});
+
+function showData(pageNum,pageSize) {
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:3000/api/index/list',
+        // url: 'http://localhost:3001/api/index/list/'+pageNum+'/'+pageSize,  /*  */
+        url: 'http://localhost:3001/api/index/list',
         dataType: 'json',
         success: function (res) {
             let data = res.data.data
             console.log(data)
             $.each(data, (index, item) => {
                 $('.main ul').append(`<li class="lists">
-                <img src="${item.list_img}" alt="">
+                <img data-original="${item.list_img}" src="img/load.gif">
                 <label>
                     <b class="discount">折扣价:${item.list_price}</b>
                     <span class="price-text">原价:${item.list_uprice}</span>
                 </label>
             </li>`)
             })
+            $('.main ul img').lazyload({
+                effect: "fadeIn",
+                // threshold:200
+            })
         }
     })
-})
+}
--- 原生/路由懒加载加分页/server/server.js
+++ 原生/路由懒加载加分页/server/server.js
@@ -10,28 +10,28 @@ app.all('*', function(req, res, next) {
 	res.header('Content-Type', 'application/json;charset=utf-8');
 	next();
 });
-app.get('/api/index/list', (req, res) => {
+app.get('/api/index/list/num/size', (req, res) => {
     res.send({
         code:200,
 		data: {
 			success: true,
 			data:[
-                {list_id:1,list_name:'经典百搭型外套',list_img:'img/commodity1.jpg',list_price:349,list_uprice:228.7},
-                {list_id:2,list_name:'穿梭恋人，保暖又迷人',list_img:'img/commodity2.jpg',list_price:549,list_uprice:428.7},
-                {list_id:3,list_name:'春夏睡衣风',list_img:'img/commodity3.jpg',list_price:149,list_uprice:28.7},
-                {list_id:4,list_name:'秋季外套',list_img:'img/commodity4.jpg',list_price:649,list_uprice:528.7},
-                {list_id:5,list_name:'圆领长袖X',list_img:'img/commodity2.jpg',list_price:49,list_uprice:28.7},
-                {list_id:6,list_name:'可爱迷人百褶裙',list_img:'img/commodity3.jpg',list_price:1349,list_uprice:1228.7},
-                {list_id:7,list_name:'经典百搭型外套',list_img:'img/commodity1.jpg',list_price:349,list_uprice:228.7},
-                {list_id:8,list_name:'穿梭恋人，保暖又迷人',list_img:'img/commodity2.jpg',list_price:549,list_uprice:428.7},
-                {list_id:9,list_name:'春夏睡衣风',list_img:'img/commodity3.jpg',list_price:149,list_uprice:28.7},
-                {list_id:10,list_name:'秋季外套',list_img:'img/commodity4.jpg',list_price:649,list_uprice:528.7},
-                {list_id:11,list_name:'圆领长袖X',list_img:'img/commodity2.jpg',list_price:49,list_uprice:28.7},
-                {list_id:12,list_name:'可爱迷人百褶裙',list_img:'img/commodity3.jpg',list_price:1349,list_uprice:1228.7},
+                {list_id:1,list_name:'经典百搭型外套',list_img:'https://s5.mogucdn.com/mlcdn/c45406/200411_8b53e374e2al04l2keag269e95i24_3999x5999.jpg_320x428.v1cAC.40.webp',list_price:349,list_uprice:228.7},
+                {list_id:2,list_name:'穿梭恋人，保暖又迷人',list_img:'https://s5.mogucdn.com/mlcdn/c45406/190417_54adhdf66h7lhl2610ddb6e6cj201_640x960.jpg_320x428.v1cAC.40.webp',list_price:549,list_uprice:428.7},
+                {list_id:3,list_name:'春夏睡衣风',list_img:'https://s5.mogucdn.com/mlcdn/c45406/180927_0hjc9jk79icfffh9ddk279i726a0k_640x960.jpg_320x428.v1cAC.40.webp',list_price:149,list_uprice:28.7},
+                {list_id:4,list_name:'秋季外套',list_img:'https://s5.mogucdn.com/mlcdn/c45406/200413_2edd3hh971g6h8edjj43l72160k8d_3999x5999.jpg_320x428.v1cAC.40.webp',list_price:649,list_uprice:528.7},
+                {list_id:5,list_name:'圆领长袖X',list_img:'https://s5.mogucdn.com/mlcdn/55cf19/190707_81kjl3e1b4jf558kcj5d66g7cf9a3_640x960.jpg_320x428.v1cAC.40.webp',list_price:49,list_uprice:28.7},
+                {list_id:6,list_name:'可爱迷人百褶裙',list_img:'https://s5.mogucdn.com/mlcdn/c45406/190812_44ik9agich7i7fg7c33761gi9b91d_640x960.jpg_320x428.v1cAC.40.webp',list_price:1349,list_uprice:1228.7},
+                {list_id:7,list_name:'经典百搭型外套',list_img:'https://s5.mogucdn.com/mlcdn/c45406/190901_826e2cca3dg0ajd5d4f7602ghcg4l_640x960.jpg_320x428.v1cAC.40.webp',list_price:349,list_uprice:228.7},
+                {list_id:8,list_name:'穿梭恋人，保暖又迷人',list_img:'https://s5.mogucdn.com/mlcdn/55cf19/191119_6j509b6glid8j6gh8cga3c3de5f3k_800x800.jpg_320x428.v1cAC.40.webp',list_price:549,list_uprice:428.7},
+                {list_id:9,list_name:'春夏睡衣风',list_img:'https://s5.mogucdn.com/mlcdn/c45406/190901_0hg425h34f87a5fhdk78353dh68b2_640x960.jpg_320x428.v1cAC.40.webp',list_price:149,list_uprice:28.7},
+                {list_id:10,list_name:'秋季外套',list_img:'https://s5.mogucdn.com/mlcdn/c45406/190619_2d2868hl06876i2i5gi0kdgdg49cg_640x960.jpg_320x428.v1cAC.40.webp',list_price:649,list_uprice:528.7},
+                {list_id:11,list_name:'圆领长袖X',list_img:'https://s5.mogucdn.com/mlcdn/c45406/200314_6l8jjdll6a4ke33kef04f8h89k9hb_640x960.jpg_320x428.v1cAC.40.webp',list_price:49,list_uprice:28.7},
+                {list_id:12,list_name:'可爱迷人百褶裙',list_img:'https://s5.mogucdn.com/mlcdn/17f85e/200402_32cc5ebjf7ajalhd41126fb9cbf80_640x960.jpg_320x428.v1cAC.40.webp',list_price:1349,list_uprice:1228.7},
             ]
 		}
     })
 })
-app.listen(3000, function () {
+app.listen(3001, function () {
     console.log('服务器启动，端口3000')
 })
