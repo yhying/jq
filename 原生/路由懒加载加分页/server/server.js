4d5be2b1eb0fce069b82bc35c92ce32e9daad127
18627805032@163.com <18627805032@163.com>
2020-04-23 17:09:18 +0800

    node后端搭建

--- 原生/路由懒加载加分页/server/server.js
+++ 原生/路由懒加载加分页/server/server.js
@@ -22,6 +22,12 @@ app.get('/api/index/list', (req, res) => {
                 {list_id:4,list_name:'秋季外套',list_img:'img/commodity4.jpg',list_price:649,list_uprice:528.7},
                 {list_id:5,list_name:'圆领长袖X',list_img:'img/commodity2.jpg',list_price:49,list_uprice:28.7},
                 {list_id:6,list_name:'可爱迷人百褶裙',list_img:'img/commodity3.jpg',list_price:1349,list_uprice:1228.7},
+                {list_id:7,list_name:'经典百搭型外套',list_img:'img/commodity1.jpg',list_price:349,list_uprice:228.7},
+                {list_id:8,list_name:'穿梭恋人，保暖又迷人',list_img:'img/commodity2.jpg',list_price:549,list_uprice:428.7},
+                {list_id:9,list_name:'春夏睡衣风',list_img:'img/commodity3.jpg',list_price:149,list_uprice:28.7},
+                {list_id:10,list_name:'秋季外套',list_img:'img/commodity4.jpg',list_price:649,list_uprice:528.7},
+                {list_id:11,list_name:'圆领长袖X',list_img:'img/commodity2.jpg',list_price:49,list_uprice:28.7},
+                {list_id:12,list_name:'可爱迷人百褶裙',list_img:'img/commodity3.jpg',list_price:1349,list_uprice:1228.7},
             ]
 		}
     })
